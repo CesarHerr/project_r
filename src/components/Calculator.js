ac2047689e4af8a2523abe04d0c53767546b9903
Cesar Herrera <cesarherr321@gmail.com>
2023-10-08 17:12:40 -0300

    Add install react config and linters

---
+++ src/components/Calculator.js
@@ -0,0 +1,9 @@
+function Calculator() {
+  return (
+    <section className="container border border-red-500 mx-auto text-center ">
+      <p>Hola que tal</p> 
+    </section>
+  );
+}
+
+export default Calculator;
